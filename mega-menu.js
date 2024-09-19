ca69658664b1cbcad2b020864c5abc94c058c166
slava-solovyev <107420723+slava-solovyev@users.noreply.github.com>
2024-09-19 19:25:35 +0300

    Update mega-menu.js
    Latest version of the Mega Menu. Still needs to be worked on

--- mega-menu.js
+++ mega-menu.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
   let megaMenuItem = $(".mega-menu_item");
   let megaMenuCmsRow = $(".mega-menu_cms_row");
   let megaMenuBlur = $(".navbar_mega-menu_blur");
-  let textLink = $(".mega-menu_socials_list").find(".text-link");
-  let localesItem = $(".navbar_mega-menu_wrap").find(".w-locales-item");
+  let textLink = $(".navbar_mega-menu_wrap").find(".text-link_wrap");
+  //let localesItem = $(".navbar_mega-menu_wrap").find(".w-locales-item");
   let navbarMenuBg = $(".navbar_menu_bg");
 
   function remToPixels(rem) {
@@ -24,7 +24,7 @@ $(document).ready(function () {
     .set(textLink, { opacity: 0 })
     .set(megaMenuBlur, { opacity: 0 }) // Set initial opacity of mega-menu_cms_row to 0
     .set(menuItem, { opacity: 100, pointerEvents: "auto" }) // Set initial opacity of mega-menu_cms_row to 0
-    .set(localesItem, { opacity: 0 }) // Set initial opacity of mega-menu_cms_row to 0
+    //.set(localesItem, { opacity: 0 }) // Set initial opacity of mega-menu_cms_row to 0
     .set(navbarMenuBg, { opacity: 1 }) // Set initial opacity of mega-menu_cms_row to 0
     .to(body, { overflow: "hidden", duration: 0 }, "0") // Disable scroll when menu is open
     .to(
@@ -32,7 +32,7 @@ $(document).ready(function () {
       {
         display: "block",
         opacity: 1,
-        duration: 0.5,
+        duration: 0.3,
       },
       "0"
     ) // Animate navbar_blur opacity to 100%
@@ -40,7 +40,7 @@ $(document).ready(function () {
       navbarMenuBg,
       {
         opacity: 0,
-        duration: 0.5,
+        duration: 0.3,
       },
       "0"
     ) // Animate background color
@@ -51,20 +51,20 @@ $(document).ready(function () {
         opacity: 0,
         stagger: 0.1,
         pointerEvents: "none",
-        duration: 0.5,
+        duration: 0.3,
       },
       "<0"
     ) // Animate menu items
-    .to(megaMenu, { display: "flex", top: "0vh", duration: 0.5 }, "<0.3") // Animate navbar_mega-menu_wrap down by 100vh
+    .to(megaMenu, { display: "flex", top: "0vh", duration: 0.3 }, "<0.3") // Animate navbar_mega-menu_wrap down by 100vh
     .to(megaMenuItem, {
       opacity: 1,
-      duration: 0.5,
+      duration: 0.3,
       top: "0rem",
       stagger: 0.1,
     }) // Stagger animate mega-menu_item opacity to 100%
-    .to(megaMenuCmsRow, { opacity: 1, duration: 0.5, stagger: 0.25 }, "<0.2")
-    .to(textLink, { opacity: 1, duration: 0.5, stagger: 0.25 }, "<0")
-    .to(localesItem, { opacity: 1, duration: 0.5, stagger: 0.25 }, "<0.4");
+    .to(megaMenuCmsRow, { opacity: 1, duration: 0.4, stagger: 0.25 }, "<0.2")
+    .to(textLink, { opacity: 1, duration: 0.3, stagger: 0.25 }, "<0");
+  //.to(localesItem, { opacity: 1, duration: 0.3, stagger: 0.25 }, "<0.4");
 
   // Click event to toggle the timeline and theme
   $(".is-menu").click(function () {
