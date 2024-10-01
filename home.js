a6852d49654a7d897ad0416b4255c5d83eac000d
Thierry François <thi3rrr@gmail.Com>
2024-10-01 09:14:49 +0300

    Create home.js
    hero video play button

---
+++ home.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    const isReduced = window.matchMedia('(prefers-reduced-motion: reduce)') === true || window.matchMedia('(prefers-reduced-motion: reduce)').matches === true;
+
+    if (!!isReduced) {
+        let iframe = $('#hero_video iframe');
+        let player = new Vimeo.Player(iframe);
+        let play = $('#hero_video_play');
+        play.show().addEventListener('click', function() {
+            player.play();
+        });
+    }
+});
