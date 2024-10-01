81c8e873928fdbd885a8d6f7a0649fe6a1ed69b7
Thierry François <thi3rrr@gmail.Com>
2024-10-01 10:26:50 +0300

    Update home.js
    fix length test

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     let iframe = $('#hero_video iframe');
     const isReduced = window.matchMedia('(prefers-reduced-motion: reduce)') === true || window.matchMedia('(prefers-reduced-motion: reduce)').matches === true;
 
-    if (!!isReduced && iframe) {
+    if (!!isReduced && iframe.length > 0) {
         
         let player = new Vimeo.Player(iframe);
         let play = $('#hero_video_play');
