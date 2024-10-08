a8897ce1ebd2387030db81f34df586321f7c8967
slava-solovyev <107420723+slava-solovyev@users.noreply.github.com>
2024-10-08 16:06:09 +0300

    Update card-slider.js
    Update the card-slider.js - now it has responsive gap between the slider and get initialised on screen resize to make sure it doesn't get broken

--- src/card-slider.js
+++ src/card-slider.js
@@ -1,37 +1,63 @@
-// Sliders and carousels functionality
+// Function to get the computed font-size of the HTML element
+function getComputedFontSize() {
+  return parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+}
 
-const rem = 16;
+// Function to initialize swiper
+function initSwiper($slider) {
+  const baseRem = getComputedFontSize(); // Get the actual HTML font-size
 
-$(".slider").each(function (index) {
-  const swiper = new Swiper($(this).find(".swiper")[0], {
+  const swiper = new Swiper($slider.find(".swiper")[0], {
     speed: 400,
     loop: false,
-    //slidesPerView: 3,
-    spaceBetween: rem * 3,
+    spaceBetween: baseRem * 3, // Use the dynamic baseRem for spaceBetween
     allowTouchMove: true,
-    //mousewheel: true,
+    // mousewheel: true,
 
     breakpoints: {
       320: {
         slidesPerView: 1.1,
-        spaceBetween: rem * 2,
+        spaceBetween: baseRem * 2,
       },
       480: {
         slidesPerView: 1.2,
-        spaceBetween: rem * 2,
+        spaceBetween: baseRem * 2,
       },
       768: {
         slidesPerView: 1.42,
+        spaceBetween: baseRem * 3,
       },
       992: {
+        slidesPerView: 2,
+        spaceBetween: baseRem * 3,
+      },
+      1241: {
         slidesPerView: 3,
+        spaceBetween: baseRem * 3,
       },
     },
 
     navigation: {
-      nextEl: $(this).find(".next")[0],
-      prevEl: $(this).find(".prev")[0],
+      nextEl: $slider.find(".next")[0],
+      prevEl: $slider.find(".prev")[0],
       disabledClass: "is-disabled",
     },
   });
+
+  return swiper;
+}
+
+// Sliders and carousels functionality
+$(".slider").each(function (index) {
+  const $slider = $(this);
+  let swiperInstance = initSwiper($slider);
+
+  // Refresh Swiper on window resize
+  $(window).on('resize', function() {
+    if (swiperInstance) {
+      swiperInstance.destroy(true, true); // Destroy the current Swiper instance
+    }
+    swiperInstance = initSwiper($slider); // Reinitialize Swiper with updated settings
+  });
 });
+
